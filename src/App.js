942c4f6eeac0add48127e491a4cfb94c046745c1
SaroshAD <saroshahmed.07@gmail.com>
2022-02-27 01:44:19 +0500

    ADDED TEXT

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Assignment <code>2</code> 
           <br/>
-            SAROSH AHMED.
+            SAROSH AHMED
         </p>
         <a
           className="App-link"
